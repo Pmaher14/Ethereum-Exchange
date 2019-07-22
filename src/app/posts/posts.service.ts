c7be36fe37f6132779b85ecd12e955f2d8c10842
Patrick <patrickmaher14@gmail.com>
2019-07-21 20:48:42 -0700

    initial commit

---
+++ src/app/posts/posts.service.ts
@@ -0,0 +1,94 @@
+import { Injectable, ɵConsole } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
+
+import { environment } from "../../environments/environment";
+import { Post } from "./post.model";
+
+const BACKEND_URL = environment.apiUrl + "/posts/";
+
+@Injectable({ providedIn: "root" })
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<{ posts: Post[]; postCount: number }>();
+
+  constructor(private http: HttpClient, private router: Router) {}
+
+  getPosts(postsPerPage: number, currentPage: number) {
+    const queryParams = `?pagesize=${postsPerPage}&page=${currentPage}`;
+    this.http
+      .get<{ message: string; posts: any; maxPosts: number }>(
+        BACKEND_URL + queryParams
+      )
+      .pipe(
+        map(postData => {
+          return {
+            posts: postData.posts.map(post => {
+              return {
+                title: post.title,
+                content: post.content,
+                id: post._id,
+                creator: post.creator
+              };
+            }),
+            maxPosts: postData.maxPosts
+          };
+        })
+      )
+      .subscribe(transformedPostData => {
+        this.posts = transformedPostData.posts;
+        this.postsUpdated.next({
+          posts: [...this.posts],
+          postCount: transformedPostData.maxPosts
+        });
+      });
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  getPost(id: string) {
+    return this.http.get<{
+      _id: string;
+      title: string;
+      content: string;
+      creator: string;
+    }>(BACKEND_URL + id);
+  }
+
+  addPost(title: string, content: string) {
+    const postData = new FormData();
+    postData.append("title", title);
+    postData.append("content", content);
+    this.http
+      .post<{ message: string; post: Post }>(
+        BACKEND_URL,
+        postData
+      )
+      .subscribe(responseData => {
+        this.router.navigate(["/"]);
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    let postData: Post | FormData;
+    postData = {
+      id: id,
+      title: title,
+      content: content,
+      creator: null
+    }
+    this.http
+      .put(BACKEND_URL + id, postData)
+      .subscribe(response => {
+        this.router.navigate(["/"]);
+      });
+  }
+
+  deletePost(postId: string) {
+    return this.http.delete(BACKEND_URL + postId);
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: "http://meantest-env.m5qm3nbaay.us-east-1.elasticbeanstalk.com/api"
+};
